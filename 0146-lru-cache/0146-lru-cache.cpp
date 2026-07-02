5fa88a7312bd9d6f155c4020c46f940a107fe1a4
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-02 18:38:41 +0530

    Time: 123 ms (11.93%), Space: 186.7 MB (10.13%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,80 @@
+class LRUCache {
+public:
+    class Node{
+        public:
+        int key;
+        int val;
+        Node* next;
+        Node* prev;
+
+        public:
+        Node(int key1, int val1){
+            key = key1;
+            val = val1;
+        }
+    };
+
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+
+    int cap;
+
+    unordered_map <int, Node*> mpp;
+
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;    
+    }
+
+    void insertNodeAfterHead(Node* newNode){
+        Node* temp = head->next;
+        newNode->next = temp;
+        newNode->prev = head;
+        head->next = newNode;
+        temp->prev = newNode;
+    }
+
+    void deleteNode(Node* delNode){
+        Node* prev = delNode->prev;
+        Node* next = delNode->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+    
+    int get(int key) {
+        if (mpp.find(key) != mpp.end()){
+            Node* node = mpp[key];
+            int res = node->val;
+            mpp.erase(key);
+            deleteNode(node);
+            insertNodeAfterHead(node);
+            mpp[key] = head->next;
+            return res;
+        }
+        else return -1;
+    }
+    
+    void put(int key, int value) {
+        if (mpp.find(key) != mpp.end()){
+            Node* node = mpp[key];
+            mpp.erase(key);
+            deleteNode(node);
+        }
+
+        if (mpp.size() == cap) {
+            mpp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+        
+        insertNodeAfterHead(new Node(key, value));
+        mpp[key] = head->next;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
