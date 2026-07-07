a15c7b0a95743ecb12a4701d28066f86b7352692
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-07 17:03:06 +0530

    Time: 0 ms (100%), Space: 27.2 MB (74.13%) - LeetHub

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int noPower = arr[0];
+        int power = arr[0];
+        int res = arr[0];
+
+        for(int i = 1; i < arr.size(); i++){
+            int v1 = arr[i];
+            int v2 = noPower + arr[i];
+            int v3 = noPower;
+            int v4 = power + arr[i];
+
+            noPower = max(v1,v2);
+            power = max(v3,v4);
+
+            res = max(res, max(noPower, power));
+
+        }
+    return res;
+    }
+};
