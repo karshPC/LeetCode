4b5f23465cdc748b49f9ef88f9004e40d6df67fa
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-21 23:46:40 +0530

    Time: 0 ms (100%), Space: 27.1 MB (75.03%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -4,8 +4,9 @@ public:
         int res = arr[0];
         int withPower = arr[0];
         int withoutPower = arr[0];
+        int n = arr.size();
 
-        for (int i = 1; i < arr.size(); i++){
+        for(int i =1; i < n; i++){
             int v1 = arr[i];
             int v2 = withoutPower + arr[i];
 
@@ -15,7 +16,7 @@ public:
             withoutPower = max(v1,v2);
             withPower = max(v3,v4);
 
-            res = max(res, max(withoutPower,withPower));
+            res = max(res, max(withPower, withoutPower));
         }
     return res;
     }
