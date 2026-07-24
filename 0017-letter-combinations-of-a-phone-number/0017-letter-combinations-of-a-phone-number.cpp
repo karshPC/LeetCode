2c5c695d7f528af1c3f89af06ff38b8574ea1935
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-24 14:51:25 +0530

    Time: 2 ms (16.11%), Space: 10.8 MB (5.07%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    vector<string> res;
 
-    void solve(int idx, string &digits, unordered_map <char, string> &mp,string &temp){
-        if (idx == digits.size()){
-            res.push_back(temp);
+    vector<string> ans;
+
+    void solve(int idx, string &digits, string &temp, unordered_map<char,string> mpp){
+        if(idx == digits.size()){
+            ans.push_back(temp);
             return;
         }
 
-        string letters = mp[digits[idx]];
+        string letters = mpp[digits[idx]];
 
         for(char ch : letters){
             temp.push_back(ch);
-            solve(idx+1, digits, mp, temp);
+            solve(idx+1, digits, temp,mpp);
             temp.pop_back();
         }
     return;
     }
 
     vector<string> letterCombinations(string digits) {
-        unordered_map <char, string> mp = {
-        {'2',"abc"},
-        {'3',"def"},
-        {'4',"ghi"},
-        {'5',"jkl"},
-        {'6',"mno"},
-        {'7',"pqrs"},
-        {'8',"tuv"},
-        {'9',"wxyz"}
+        unordered_map<char, string> mpp{
+            {'2',"abc"},
+            {'3',"def"},
+            {'4',"ghi"},
+            {'5',"jkl"},
+            {'6',"mno"},
+            {'7',"pqrs"},
+            {'8',"tuv"},
+            {'9',"wxyz"}
         };
+
+        int idx = 0;
         string temp = "";
+        solve(idx,digits, temp, mpp);
 
-        solve(0,digits, mp,temp);
-
-        return res;
+        return ans;
 
     }
 };
