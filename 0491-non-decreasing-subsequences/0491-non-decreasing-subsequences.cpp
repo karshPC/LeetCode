f60be57d8932ca20ed37020fa92afe5c518968b7
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-26 22:23:22 +0530

    Time: 24 ms (80.52%), Space: 26.4 MB (69.74%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+
     int n;
 
-    void solve(vector<int> &nums, int idx, vector<int> &curr, vector<vector<int>> &res){
-
-        if(curr.size() >= 2)
+    void solve(vector<int> &nums,int idx,vector<int> &curr, vector<vector<int>> &res){
+        if(curr.size() >= 2){
             res.push_back(curr);
+        }
 
+        if(idx == n){
+            return;
+        }
+        
         unordered_set<int> st;
 
         for(int i = idx; i < n; i++){
-            if((curr.empty() || curr.back() <= nums[i]) &&
-               st.find(nums[i]) == st.end()){
-
+            if((curr.empty() || curr.back() <= nums[i]) && (st.find(nums[i]) == st.end())){
+                
                 st.insert(nums[i]);
                 curr.push_back(nums[i]);
-                solve(nums, i + 1, curr, res);
+                solve(nums, i+1, curr, res);
                 curr.pop_back();
             }
         }
+
+    return;
     }
 
     vector<vector<int>> findSubsequences(vector<int>& nums) {
+        int idx = 0;
         n = nums.size();
         vector<int> curr;
         vector<vector<int>> res;
-        solve(nums, 0, curr, res);
-        return res;
+        solve(nums, idx, curr, res);
+    return res;
     }
 };
