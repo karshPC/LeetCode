6ce8071f20b5070ccedbf5c78f657878e2754b5c
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-07-31 18:45:58 +0530

    Time: 0 ms (100%), Space: 15.9 MB (54.85%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -6,38 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-
-    void solve(TreeNode* node, vector<vector<int>> &res){
-        
+    void solve(TreeNode* node, deque<vector<int>>& res) {
         queue<TreeNode*> q;
         q.push(node);
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int levelSize = q.size();
             vector<int> temp;
-            for(int i = 0; i < levelSize; i++){
-                TreeNode* t = q.front();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* curr = q.front();
                 q.pop();
 
-                temp.push_back(t->val);
+                temp.push_back(curr->val);
 
-                if(t->left != nullptr) q.push(t->left);
-                if(t->right != nullptr) q.push(t->right);
+                if (curr->left)
+                    q.push(curr->left);
+
+                if (curr->right)
+                    q.push(curr->right);
             }
-        res.push_back(temp);
-        }   
+
+            res.push_front(temp);
+        }
     }
 
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        if(root == nullptr) return {};
-        vector<vector<int>> res;
+        if (root == nullptr)
+            return {};
+
+        deque<vector<int>> res;
         solve(root, res);
-        reverse(res.begin(), res.end());
-    return res;
+
+        return vector<vector<int>>(res.begin(), res.end());
     }
 };
