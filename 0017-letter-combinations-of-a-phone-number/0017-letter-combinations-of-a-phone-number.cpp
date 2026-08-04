78e82a4cd96062e782f3e0b3475b6448d833eab9
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-08-04 23:26:57 +0530

    Time: 0 ms (100%), Space: 9.5 MB (20.76%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
 
-    void solve(string &digits, int idx, string &curr, vector<string> &res, unordered_map<char, string> &mpp){
+    vector<string> res;
+
+    void solve(string &digits, string &temp, int idx, unordered_map<char, string> &mpp){
+        
         if(idx == digits.size()){
-            res.push_back(curr);
+            res.push_back(temp);
             return;
         }
 
         string letters = mpp[digits[idx]];
 
         for(char letter : letters){
-            curr.push_back(letter);
-            solve(digits, idx+1, curr, res, mpp);
-            curr.pop_back();
+            temp.push_back(letter);
+            solve(digits, temp, idx+1, mpp);
+            temp.pop_back();
         }
 
     return;
     }
 
     vector<string> letterCombinations(string digits) {
-        
-        unordered_map<char, string> mpp{
+        int idx = 0;
+        unordered_map<char,string> mpp({
             {'2',"abc"},
             {'3',"def"},
             {'4',"ghi"},
-            {'5', "jkl"},
+            {'5',"jkl"},
             {'6',"mno"},
             {'7',"pqrs"},
             {'8',"tuv"},
             {'9',"wxyz"}
-        };
+        });
+        string temp;
+    solve(digits, temp, idx, mpp);
 
-        int idx = 0;
-        string curr = "";
-        vector<string> res;
-        solve(digits, idx, curr, res, mpp);
-        return res;
+    return res;
     }
 };
