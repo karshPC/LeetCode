b673cba6faa1d27f74bba409d8ddb5ae50ff4726
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-08-05 22:41:48 +0530

    Time: 3 ms (52.13%), Space: 39.2 MB (11.54%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<int> nodes;
+
+    void inOrder(TreeNode* node){
+        if(!node) return;
+
+        inOrder(node->left);
+        nodes.push_back(node->val);
+        inOrder(node->right);
+    
+    return;
+    }
+
+    vector<int> findTarget(unordered_map<int,int> &found, int n, int k){
+        for(int i = 0 ; i < n; i++){
+            int needed = k - nodes[i];
+            if(found.find(needed) != found.end()){
+                return {found[needed], i};
+            }
+            found[nodes[i]] = i;
+        }
+
+    return {};
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        inOrder(root);
+
+        unordered_map<int,int> found;        
+        int n = nodes.size();
+        vector<int> map = findTarget(found, n, k);
+        
+        return (map.size() == 2) ? true : false;
+    }
+};
