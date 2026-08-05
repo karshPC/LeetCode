e33adb8e17b163797ae6e28293b3355e46f90f0d
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-08-05 22:45:37 +0530

    Time: 9 ms (18.8%), Space: 39 MB (20.76%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -24,25 +24,20 @@ public:
     return;
     }
 
-    vector<int> findTarget(unordered_map<int,int> &found, int n, int k){
-        for(int i = 0 ; i < n; i++){
-            int needed = k - nodes[i];
-            if(found.find(needed) != found.end()){
-                return {found[needed], i};
-            }
-            found[nodes[i]] = i;
-        }
-
-    return {};
-    }
-
     bool findTarget(TreeNode* root, int k) {
         inOrder(root);
 
         unordered_map<int,int> found;        
         int n = nodes.size();
-        vector<int> res = findTarget(found, n, k);
         
-        return (res.size() == 2) ? true : false;
+        for(int i = 0 ; i < n; i++){
+            int needed = k - nodes[i];
+            if(found.find(needed) != found.end()){
+                return true;
+            }
+            found[nodes[i]] = i;
+        }
+
+    return false;       
     }
 };
