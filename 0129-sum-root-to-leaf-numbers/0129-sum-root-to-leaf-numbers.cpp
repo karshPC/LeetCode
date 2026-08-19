07f2fda99973e22c1fb0461297db48d8e16c295c
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-08-20 00:15:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (13.78%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,20 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
 
-    int solve(TreeNode* node, int currentSum){
-        if(node == nullptr) return 0;
+    int sum =0;
+    vector<int> diary;
 
-        currentSum = currentSum * 10 + node->val;
-        
+
+    void calculateSum(){
+        int n = diary.size();
+        for(int i = 0; i < n; i++){
+            sum += diary[i];
+        }
+    return;
+    }
+
+    void solve(TreeNode* node, int localSum){
+        if(!node) return;
+
+        localSum = localSum * 10 + node->val;
+
         if(node->left == nullptr && node->right == nullptr){
-            return currentSum;
+            diary.push_back(localSum);
         }
 
-        return solve(node->left, currentSum) + solve(node->right, currentSum);
+        solve(node->left, localSum);
+        solve(node->right, localSum);
+
+    return;
     }
 
     int sumNumbers(TreeNode* root) {
-        int res = solve(root, 0);
-    return res;
+        solve(root, 0);
+        calculateSum();
+    return sum;
     }
 };
