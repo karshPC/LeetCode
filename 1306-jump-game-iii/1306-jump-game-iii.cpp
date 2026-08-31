8736c03a4da41df4670cfaf23fc5cdec9271919a
Utkarsh Sharma <sharmautkarsh2992@gmail.com>
2026-08-31 23:51:23 +0530

    Time: 0 ms (100%), Space: 30.6 MB (74.4%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<bool> visited(n, false);
+
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            if (arr[i] == 0)
+                return true;
+
+            int left = i - arr[i];
+            int right = i + arr[i];
+
+            if (left >= 0 && !visited[left]) {
+                visited[left] = true;
+                q.push(left);
+            }
+
+            if (right < n && !visited[right]) {
+                visited[right] = true;
+                q.push(right);
+            }
+        }
+
+        return false;
+    }
+};
